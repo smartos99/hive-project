6079de636e6165ff778faed4f1cf485e091007b7
huertaarielcsw <huertaarielcsw@gmail.com>
2021-12-08 06:13:55 +0100

    Work almost perfectly Player vs Player with expansions

--- game_logic/gameBoard.pl
+++ game_logic/gameBoard.pl
@@ -1,8 +1,14 @@
-:- module(gameBoard, [neighbor_n/2, neighbor_s/2, neighbor_ne/2, neighbor_se/2,
-                      neighbor_nw/2, neighbor_sw/2, get_neighbor_cell/2,
-                      get_neighborhood/2, valid_cell/2,
-                      connected_to_hive_without_origin_cell/2, hamiltonian_path/1,
-                      is_surrounded/1, connected_to_hive/1]).
+:- module(gameBoard, [bridge/3, neighbor_n/2, neighbor_s/2, neighbor_ne/2,
+                      neighbor_se/2, neighbor_nw/2, neighbor_sw/2,
+                      get_neighbor_cell/2, get_neighborhood/2, valid_cell/2,
+                      connected_to_hive_without_origin_cell/2, can_slide/2 ,
+                      stunne_cell/2, remove_stunne/2, is_last/2,
+                      hamiltonian_path/1, is_stunned/1, last_cell_placed/2,
+                      is_surrounded/1, connected_to_hive/1,update_last_cell/2,
+                      update_stunned_pieces/0]).
+
+:- dynamic stunned/2.
+:- dynamic last/2.
 
 :- [cell].
 %                         N
@@ -46,7 +52,7 @@ neighbor_se(cell(_,_,_,Row,Column),Ncell) :-
     (
      cell:get_cell(cell(Bug,Color,PiledUp,Row,Ncolumn)),
      Ncell = cell(Bug,Color,PiledUp,Row,Ncolumn),!;
-     Ncell = cell(none,none,0,Row,Column)
+     Ncell = cell(none,none,0,Row,Ncolumn)
      ).
 %devuelve el vecino al noroeste de la celda declarada
 neighbor_nw(cell(_,_,_,Row,Column),Ncell) :-
@@ -78,9 +84,13 @@ get_neighbor_cell(C,Ncell) :-
 %devuelve los seis vecinos de la celda
 get_neighborhood(C, Ncells) :- findall(Ncell, get_neighbor_cell(C, Ncell), Ncells).
 
-is_surrounded(cell(Bug,Color,PiledUp,Row,Column)) :-
-    get_neighborhood(cell(Bug,Color,PiledUp,Row,Column), NCells),
-    \+member(cell(none,none,_,_,_),NCells).
+is_surrounded(Cell) :-
+    findall(NCell,neighbors_non_empty(Cell,NCell),NCells),
+    length(NCells,6).
+
+neighbors_non_empty(Cell,NCell) :-
+    get_neighbor_cell(Cell,NCell),
+    \+empty_cell(NCell).
 
 neighbors_non_visited(Cell,Visited,Ncell):-
     get_neighbor_cell(Cell,Ncell),
@@ -90,8 +100,8 @@ neighbors_non_visited(Cell,Visited,Ncell):-
 dfs([],Visited,Visited) :- !.
 dfs([FCell|Cells],Visited,Path) :-
     !,
-    dfs(FCell,Visited,NVisited),
-    dfs(Cells,[FCell|NVisited],Path).
+    dfs(FCell,[FCell|Visited],NVisited),
+    dfs(Cells,NVisited,Path).
 dfs(Cell,Visited,Path) :-
     findall(Ncell, neighbors_non_visited(Cell,Visited,Ncell), Ncells),
     %append(NCells, Visited, NVisited),
@@ -99,40 +109,78 @@ dfs(Cell,Visited,Path) :-
 
 hamiltonian_path(cell(Bug,Color,PiledUp,Row,Column)) :-
     remove_cell(Bug,Color,PiledUp,Row,Column),
-    get_cells_list([FCell|Cells]),
+    get_top_cells_list([FCell|Cells]),
     dfs(FCell,[FCell],Path),
     add_cell(Bug,Color,PiledUp,Row,Column),
-    length([FCell|Cells],L),
-    length(Path,L).
+    forall(member(MCell,[FCell|Cells]), member(MCell,Path)).
+    %length([FCell|Cells],L),
+    %length(Path,L).
 
 valid_cell(Color,Cell) :-
     get_cell(cell(Bug,Color,PiledUp,Row,Column)),
     PiledUp = 0,
-    get_neighbor_cell(cell(Bug,Color,PiledUp,Row,Column),Cell),
-    empty_cell(Cell),
-    get_neighborhood(Cell,NCells),
-    same_color_neighbor(Color,NCells).
+    get_neighbor_cell(cell(Bug,Color,PiledUp,Row,Column),VCell),
+    empty_cell(VCell),
+    get_neighborhood(VCell,NCells),
+    forall(member(NCell,NCells),same_color_neighbor(Color,NCell)),
+    Cell = VCell.
 
-same_color_neighbor(Color,[]) :- Color.
-same_color_neighbor(Color,[cell(_,C,PiledUp,Row,Column)|Cells]) :-
-    (
-     (
-      PiledUp > 0,
-      get_cell(cell(_,PC,0,Row,Column)),!,
-      PC is Color
-     );
-     C is Color;
-     C is none
-    ),
-    same_color_neighbor(Color,Cells).
+same_color_neighbor(Color,cell(_,C,_,_,_)) :-
+     C = Color;
+     C = none.
 
 connected_to_hive(Cell) :-
     get_cells_list(C),
     get_neighbor_cell(Cell,NCell),
-    member(NCell,C).
+    member(NCell,C),!.
 
 connected_to_hive_without_origin_cell(OCell,Cell):-
     get_cells_list(C),
     get_neighbor_cell(Cell,NCell),
     delete(C,OCell,NC),
-    member(NCell,NC).
+    member(NCell,NC),!.
+
+bridge(PCell,MCell,BCell) :-
+    get_neighbor_cell(MCell,NCell),
+    get_neighbor_cell(PCell,NCell),
+    \+empty_cell(NCell),
+    top_of_pile(NCell),
+    BCell = NCell.
+
+can_slide(OCell,DCell) :-
+    get_neighbor_cell(OCell,NCell),
+    get_neighbor_cell(DCell,NCell),
+    empty_cell(NCell).
+
+is_stunned(Cell) :- stunned(Cell,_).
+get_stunned_cell(Cell,Turn) :- stunned(Cell,Turn).
+stunne_cell(Cell,Turn) :- assertz(stunned(Cell,Turn)).
+remove_stunne(Cell,Turn) :- retract(stunned(Cell,Turn)).
+update_stunned_pieces :-
+    findall((Cell,Turn),get_stunned_cell(Cell,Turn), Cells),
+    update_turn_count(Cells).
+
+update_turn_count([(Cell,Turn)|Cells]) :-
+    NTurn is Turn - 1,
+    (
+      (
+          NTurn =:= 0,
+          remove_stunne(Cell,Turn)
+      );
+      (
+          NTurn > 0,
+          remove_stunne(Cell,Turn),
+          stunne_cell(Cell,NTurn)
+      )
+     ),
+    update_turn_count(Cells).
+
+update_turn_count([]) :- !.
+
+
+is_last(Cell, Color) :- last(Cell,Color).
+last_cell_placed(Cell,Color) :- assertz(last(Cell,Color)).
+update_last_cell(NCell,Color) :-
+    last(Cell,Color),
+    retract(last(Cell,Color));
+    last_cell_placed(NCell,Color).
--- game_logic/test.pl
+++ game_logic/test.pl
@@ -2,66 +2,63 @@
 :- [gameBoard].
 :- [deck].
 :- [instructions].
-
-bugs(B) :- B = [ant, grasshopper, beetle, spider, queenbee, mosquito, ladybug, pillbug].
-%borrador
-initiate_PvsP :- add_deck(white,3,3,2,2,1,1,1,1), add_deck(black,3,3,2,2,0,1,1,1).
-initiate_PvsIA :- add_deck(white,3,3,2,2,1,1,1,1), add_deck(black,3,3,2,2,1,1,1,1).
-
-%el jugador selcciona una ficha a mover
-piece_selection(Color) :-
-    repeat,
-    write("\nEnter Piece Location in form 'd_bug.' (where d is for deck and bug is de name of the bug selected) or 'b_row_column. (where b is for board and row and column is the coordenates of the piece in the board.)' \n"),
-    read(Input),
-    string_lower(Input,Processed),
-    write(Processed),
-    split_string(Processed, "_", "", L),
-    write(L),
-    parse_input(Color,L).
-
-parse_input(Color,[T|L]) :-
-    string_chars(T,[Choice]),
-    (
-     (
-      Choice = d,
-      write(L),
-      deck_piece(Color,L)
-     );
-     (
-      Choice = b,
-      write(L),
-      board_piece(Color,L)
-     )
-    ).
-
-% comprobar que sea un bicho valido y q haya al menos uno en la mano y
-% retornar movimientos validos.
-deck_piece(Color, [Bug]) :-
-   bugs(B),
-   write(Bug),
-   atom_string(SBug,Bug),
-   member(SBug, B),
-   write(B),
-   write(SBug),
-   get_bug_count(Color,SBug,Count),
-   write(Count),
-   Count > 0,
-   \+is_piled_up(queenbee,Color,_,_).
+:- [main].
 
 testing(P) :-
-        initiate_PvsP,
+        %initiate_PvsP,
         %get_bug_count(black,mosquito,Count),
         %write(Count),
         %add_cell(queenbee,white,1,3,2),
         %get_cell(cell(queenbee,white,PiledUp,Row,Column)),
         %piece_selection(black),
         %instructions(Game),
-        add_cell(ladybug,white,0,5,5),
-	add_cell(ant,white,0,4,5),
-        add_cell(ant,white,0,4,6),
-        add_cell(ant,white,0,5,4),
+        %add_cell(ladybug,white,0,5,5),
+	%add_cell(ant,white,0,4,5),
+        %add_cell(ant,white,0,4,6),
+        %add_cell(ant,white,0,5,4),
         %get_neighborhood(cell(ant,white,0,1,1),Ncells),
 	%get_cells_list(P),
         %hamiltonian_path(cell(ladybug,white,0,5,5)),
-        cell(ant,white,0,5,4) \== cell(ant,white,0,4,6).
+        %cell(ant,white,0,5,4) \== cell(ant,white,0,4,6).
+        cleanup_decks,
+        cleanup_cells,
+        %add_cell(spider,white,0,17,13),
+        %add_cell(queenbee,white,0,18,12),
+        %add_cell(ant,white,0,15,15),
+        %add_cell(ant,white,0,14,15),
+        %add_cell(ant,white,0,16,15),
+        %add_cell(ant,white,0,14,16),
+        %add_cell(ant,white,0,15,16),
+        %add_cell(ant,white,0,15,14),
+        %add_cell(ant,white,0,16,14),
+        %is_surrounded(cell(queenbee,white,0,18,12)),
+        %start
+        add_cell(ladybug,black,0,16,13),
+        add_cell(queenbee,black,0,15,14),
+        add_cell(queenbee,white,2,17,13),
+        add_cell(ant,white,0,18,12),
+        add_cell(spider,white,0,19,12),
+        add_cell(grasshopper,white,0,16,15),
+        add_cell(ant,white,2,15,15),
+        add_cell(beetle,white,1,15,15),
+        add_cell(beetle,black,0,15,15),
+        add_cell(beetle,black,1,17,13),
+        add_cell(mosquito,black,0,17,13),
+        add_cell(pillbug,black,0,17,14),
+        add_deck(white,1,3,0,1,0,1,1,1),
+        add_deck(black,3,2,1,2,0,0,0,0),
+        %stunne_cell(cell(pillbug,black,0,17,14),2),
+        stunne_cell(cell(grasshopper,white,0,16,15),1),
+        %is_stunned(cell(pillbug,black,0,17,14)),
+        %\+is_stunned(cell(beetle,black,1,17,13)),
+        %update_stunned_pieces,
+        %is_stunned(cell(pillbug,black,0,17,14)),
+        %\+is_stunned(cell(cell(mosquito,black,0,17,13))),
+        %last_cell_placed(cell(pillbug,black,0,17,14)),
+        %is_last(cell(pillbug,black,0,17,14)),
+        %\+is_last(cell(mosquito,black,0,17,13)),
+        %update_last_cell(cell(mosquito,black,0,17,13)),
+        %is_last(cell(mosquito,black,0,17,13)),
+        %is_last(cell(pillbug,black,0,17,14)),
+        play_game(black,11).
 
